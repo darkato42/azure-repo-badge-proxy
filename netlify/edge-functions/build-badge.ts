f93ad7e77c6a993c3a2d8bd6cbf4e864fa7c1d2a
Yulin Wu <darkato@icloud.com>
2022-07-25 13:27:27 +0800

    test azure badge

--- netlify/edge-functions/build-badge.ts
+++ netlify/edge-functions/build-badge.ts
@@ -1,17 +1,20 @@
 import { Context } from "netlify:edge";
 
+
+
 export default async (request: Request, context: Context) => {
 
     const pat = Deno.env.get("AZURE_PAT");
+    const base64PAT = btoa(":" +pat);
+    
 
-    return new Response(`${pat}`);
+    var uri = "https://dev.azure.com/inv-willistowerswatson/SIMS/_apis/build/status/74?api-version=6.0-preview.1&label=EsgEngine v1.0.0-beta.0";
 
-    const joke = await fetch("https://icanhazdadjoke.com/", {
+    const response = await fetch(uri, {
         "headers": {
-            "Accept": "application/json"
+            "Authorization": "Basic " + base64PAT
         }
     });
 
-    const jsonData = await joke.json();
-    return context.json(jsonData);
+    return response;
 };
