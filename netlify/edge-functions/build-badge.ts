0113b306ba208532cfd9f482dd96d5c5d34e6e03
Yulin Wu <darkato@icloud.com>
2022-07-25 21:22:30 +0800

    add badge fetching feature

--- netlify/edge-functions/build-badge.ts
+++ netlify/edge-functions/build-badge.ts
@@ -1,20 +1,62 @@
 import { Context } from "netlify:edge";
 
-
-
 export default async (request: Request, context: Context) => {
 
+    // Convert PAT for Basic Auth
     const pat = Deno.env.get("AZURE_PAT");
-    const base64PAT = btoa(":" +pat);
+    if(!pat){
+        console.log("No PAT found. Please add an environment variable called AZURE_PAT");
+        return;
+    }
+
+    const base64PAT = btoa(":" + pat);
     
+    // Extract query params from request url
+    const requestUri = new URL(request.url);
+    const pathRegex = /\/fetch-azure-badge\/(.+?)\/(.+?)\/(\d+)/;
+    const match = requestUri.pathname.match(pathRegex);
+    if (!match) {
+        console.log('no match');
+        return;
+    }
+    const org = match[1];
+    const project = match[2];
+    const buildDefinitionId = match[3];
 
-    var uri = "https://dev.azure.com/inv-willistowerswatson/SIMS/_apis/build/status/74?api-version=6.0-preview.1&label=EsgEngine v1.0.0-beta.0";
+    const params = new URLSearchParams(requestUri.search);
+    const name = params.get('name');
+    const buildNumberFilter = params.get('buildNumber');
+    const tagFilters = params.get("tagFilters");
+    
+    // Get the latest build info
+    var urlBuildInfo = `https://dev.azure.com/${org}/${project}/_apis/build/builds?definitions=${buildDefinitionId}&$top=1`
 
-    const response = await fetch(uri, {
+    if(buildNumberFilter)
+        urlBuildInfo += `&buildNumber=${buildNumberFilter}`;
+
+    if (tagFilters)
+        urlBuildInfo += `&tagFilters=${tagFilters}`;
+
+    console.log("urlBuildInfo: " + urlBuildInfo);
+
+    const respBuildInfo = await fetch(urlBuildInfo, {
+        "headers": {
+            "Authorization": "Basic " + base64PAT,
+            "Accept": "application/json"
+        }
+    });
+    
+    const buildInfoJson = await respBuildInfo.json();
+    const semVer = buildInfoJson.value[0].buildNumber;
+    const sourceBranch = buildInfoJson.value[0].sourceBranch;
+
+    // Fetch the badge
+    const urlBuildBadge = `https://dev.azure.com/${org}/${project}/_apis/build/status/${buildDefinitionId}?branchName=${sourceBranch}&label=${name} ${semVer}`;
+    console.log("urlBuildBadge: " + urlBuildBadge);
+    const respBuildBadge = await fetch(urlBuildBadge, {
         "headers": {
             "Authorization": "Basic " + base64PAT
         }
     });
-
-    return response;
+    return respBuildBadge;
 };
