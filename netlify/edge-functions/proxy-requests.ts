cbc9fabee1f5d52fa7da3ae83264961739457ef9
Yulin Wu <darkato@icloud.com>
2022-07-25 12:41:34 +0800

    init commit

---
+++ netlify/edge-functions/proxy-requests.ts
@@ -0,0 +1,13 @@
+import { Context } from "netlify:edge";
+
+export default async (request: Request, context: Context) => {
+
+  const joke = await fetch("https://icanhazdadjoke.com/", {
+    "headers": {
+      "Accept": "application/json"
+    }
+  });
+  
+  const jsonData = await joke.json();
+  return context.json(jsonData);
+};
